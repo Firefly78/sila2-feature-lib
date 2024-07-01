bc68982986a5d52dc57f562b7cc2096d488cce87
Erik Trygg <etrg@novonordisk.com>
2024-07-01 11:04:40 +0200

    feat: Added helper function for dynamic module import.

--- src/sila2_feature_lib/__init__.py
+++ src/sila2_feature_lib/__init__.py
@@ -1,6 +1,7 @@
-from .load import get_xml, get_xml_path
+from .load import dynamic_import, get_xml, get_xml_path
 
 __all__ = [
+    "dynamic_import",
     "get_xml",
     "get_xml_path",
 ]
--- src/sila2_feature_lib/load.py
+++ src/sila2_feature_lib/load.py
@@ -3,6 +3,35 @@ from __future__ import annotations
 from pathlib import Path
 
 
+def dynamic_import(name: str) -> any:
+    """Dynamically load an item from a python module.
+
+    ```python
+    # Import example:
+    my_feature = dynamic_import("my_module:my_feature")
+    server.add(my_feature()) # Instantiate and add the feature to the server
+
+    # Content of my_module.py:
+    class my_feature(sila.Feature):
+        pass
+    ```
+
+
+    Args:
+        name (str): The name of the item to load in format 'module:item'."""
+    from importlib import import_module
+
+    try:
+        module, item = name.split(":")
+    except ValueError:
+        raise ValueError(f"Invalid name '{name}'. Need to be in format 'module:item'")
+
+    try:
+        return getattr(import_module(module), item)
+    except AttributeError as ex:
+        raise AttributeError(f"Item '{item}' not found in module '{module}'") from ex
+
+
 def get_xml(name: str, version: str) -> str:
     return get_xml_path(name, version).read_text()
 
