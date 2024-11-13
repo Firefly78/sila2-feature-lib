e4065bcf9f4f322694f435b85db338f6def874dc
Erik Trygg <etrg@novonordisk.com>
2024-11-13 10:28:03 +0100

    fix: Issue where pyyaml was unintentionally required when using the resource feature.

---
+++ examples/resource_server.py
@@ -0,0 +1,5 @@
+from sila2_feature_lib.resource.v001_0.feature_ul import ResourcesService
+
+# Test config imports work
+ResourcesService("examples/data/resource_config.json")
+ResourcesService("examples/data/resource_config.yaml")
