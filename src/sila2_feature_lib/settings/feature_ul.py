eda7e41096d3cad66653d05098a3810de7da317b
Erik Trygg <etrg@novonordisk.com>
2025-03-17 11:21:05 +0100

    feat: various updates

--- src/sila2_feature_lib/settings/feature_ul.py
+++ src/sila2_feature_lib/settings/feature_ul.py
@@ -1,61 +1,61 @@
 import json
 import logging
-from typing import Any, Union
+from dataclasses import dataclass, field
+from typing import Any, Callable, List, Literal, Optional, Union
 
-from unitelabs.cdk import sila
+try:
+    from unitelabs.cdk import sila
+except ImportError as ex:
+    raise ImportError(
+        "Please install the unitelabs package by running 'pip install sila2-feature-lib[unitelabs]'"
+    ) from ex
 
 logger = logging.getLogger(__name__)
 
 
-KEY_SEPARATOR = "."
-
-
-class SettingsFeature(sila.Feature):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._settings = {}
-
-    @sila.UnobservableCommand(
-        description="Return content of the key",
+@dataclass
+class Callbacks:
+    on_register: List[Callable[["DataStore", str, Any], None]] = field(
+        default_factory=list
     )
-    def read(self, key: str) -> str:
-        """
-        .. parameter:: Key to read from the settings object
-            :name: Key
-        .. return:: JSON representation of the value
-            :name: Json
-        """
+    on_update: List[Callable[["DataStore", str, Any], None]] = field(
+        default_factory=list
+    )
+    on_reload: List[Callable[["DataStore"], None]] = field(default_factory=list)
 
+    def trigger(
+        self,
+        *args,
+        event: Literal["on_register", "on_update", "on_reload"],
+        **kwargs,
+    ):
+        for cb in getattr(self, event) or []:
+            try:
+                cb(*args, **kwargs)
+            except Exception as e:
+                logger.error(f"Error in callback {event}: {e}")
+
+
+class DataStore:
+    def __init__(self, *, key_separator="."):
+        self.key_separator = key_separator
+        self._content = {}
+        self.callbacks = Callbacks()
+
+    def read_all(self):
+        return self._content.copy()
+
+    def read(self, key: str):
         def _read(_key: str, settings: dict):
-            if KEY_SEPARATOR not in _key:
+            if self.key_separator not in _key:
                 return settings[_key]
             else:
-                _key, next = _key.split(".", maxsplit=1)
+                _key, next = _key.split(self.key_separator, maxsplit=1)
                 return _read(next, settings[_key])
 
-        return json.dumps(_read(key, self._settings))
+        return _read(key, self._content)
 
-    @sila.UnobservableCommand(
-        description="Return a copy of the settings object (JSON)",
-    )
-    def read_all(self) -> str:
-        """
-        .. return:: JSON representation of the settings object
-            :name: Json
-        """
-        return json.dumps(self._settings)
-
-    @sila.UnobservableCommand(
-        description="Register a new key-value pair, will error if key already exists",
-    )
-    def register(self, key: str, value: str):
-        """
-        .. parameter:: Target key
-            :name: Key
-        .. parameter:: Value to write to the key location
-            :name: Value
-        """
-
+    def register(self, key: str, value: Any):
         def _register(
             _key: str,
             _value: Union[str, int, float, dict[str, Any], list],
@@ -66,68 +66,128 @@ class SettingsFeature(sila.Feature):
                     f"Cannot register key in non-dict object {key.split(_key)[0]}"
                 )
 
-            if KEY_SEPARATOR not in _key:
+            if self.key_separator not in _key:
                 if _key in settings:
                     raise KeyError(f"Key '{_key}' already exists")
                 settings[_key] = _value
             else:
-                _key, next = _key.split(KEY_SEPARATOR, maxsplit=1)
+                _key, next = _key.split(self.key_separator, maxsplit=1)
                 if _key not in settings:
                     settings[_key] = {}
                 _register(next, _value, settings[_key])
 
-        _register(key, json.loads(value), self._settings)
+        _register(key, value, self._content)
+        self.callbacks.trigger(self, key, value, event="on_register")
+
+    def reload(self):
+        logger.info("Reloading settings")
+        self._content = {}  # Empty the content - default
+        self.callbacks.trigger(self, event="on_reload")
+
+    def update(self, key: str, value: Any):
+        def _update(
+            _key: str,
+            _value: Union[str, int, float, dict[str, Any], list],
+            settings: dict,
+        ):
+            if self.key_separator not in _key:
+                if _key not in settings:
+                    raise KeyError(f"Key '{_key}' in '{key}' does not exist")
+                settings[_key] = _value
+            else:
+                _key, next = _key.split(self.key_separator, maxsplit=1)
+                if _key not in settings:
+                    raise KeyError(f"Key '{_key}' in '{key}' does not exist")
+                _update(next, _value, settings[_key])
+
+        _update(key, value, self._content)
+        self.callbacks.trigger(self, key, value, event="on_update")
+
+
+def access_data_store() -> DataStore:
+    return SettingsService.get_singleton_store()
+
+
+class SettingsService(sila.Feature):
+    def __init__(
+        self,
+        *args,
+        store: Optional[DataStore] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        if store is None:
+            logger.warning("No store provided, using default")
+            store = DataStore()
+        self._store = store
+
+        SettingsService.setup_singletonian(self)
+
+    @classmethod
+    def get_singleton_store(cls) -> DataStore:
+        return cls._instance._store
+
+    @classmethod
+    def setup_singletonian(cls, instance: "SettingsService"):
+        if hasattr(cls, "_instance"):
+            raise ValueError("Only one instance of SettingsFeature is allowed")
+        cls._instance = instance
+
+    @sila.UnobservableCommand(
+        description="Return content of the key",
+    )
+    async def read(self, key: str) -> str:
+        """
+        .. parameter:: Key to read from the settings object
+            :name: Key
+        .. return:: JSON representation of the value
+            :name: Json
+        """
+
+        return json.dumps(self._store.read(key))
+
+    @sila.UnobservableCommand(
+        description="Return a copy of the settings object (JSON)",
+    )
+    async def read_all(self) -> str:
+        """
+        .. return:: JSON representation of the settings object
+            :name: Json
+        """
+        return json.dumps(self._store.read_all())
+
+    @sila.UnobservableCommand(
+        description="Register a new key-value pair, will error if key already exists",
+    )
+    async def register(self, key: str, value: str):
+        """
+        .. parameter:: Target key
+            :name: Key
+        .. parameter:: Value to write to the key location
+            :name: Value
+        """
+        logger.info(f"Registering {key} with value {value}")
+        self._store.register(key, json.loads(value))
+
+    @sila.UnobservableCommand(
+        description="Reload the settings from file",
+    )
+    async def reload(self):
+        """
+        Reload the settings from source
+        """
+        logger.info("Reloading settings...")
+        self._store.reload()
 
     @sila.UnobservableCommand(
         description="Update the value of a key",
     )
-    def update(self, key: str, value: str):
+    async def update(self, key: str, value: str):
         """
         .. parameter:: Target key to update
             :name: Key
         .. parameter:: Value to write to the key location
             :name: Value
         """
-
-        def _update(
-            _key: str,
-            _value: Union[str, int, float, dict[str, Any], list],
-            settings: dict,
-        ):
-            if KEY_SEPARATOR not in _key:
-                if _key not in settings:
-                    raise KeyError(f"Key '{_key}' in '{key}' does not exist")
-                settings[_key] = _value
-            else:
-                _key, next = _key.split(KEY_SEPARATOR, maxsplit=1)
-                if _key not in settings:
-                    raise KeyError(f"Key '{_key}' in '{key}' does not exist")
-                _update(next, _value, settings[_key])
-
-        _update(key, json.loads(value), self._settings)
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    from unitelabs.cdk import Connector
-
-    async def main():
-        app = Connector(
-            {
-                "sila_server": {
-                    "name": "Settings Server",
-                    "type": "SettingsServer",
-                    "description": "Server for settings management",
-                    "version": "1.0.0",
-                    "vendor_url": "https://www.novonordisk.com/",
-                }
-            }
-        )
-        app.register(SettingsFeature())
-
-        [setattr(f, "sila2_version", "1.0") for f in app.sila_server.features.values()]
-
-        await app.start()
-
-    asyncio.get_event_loop().run_until_complete(main())
+        logger.info(f"Updating {key} with value {value}")
+        self._store.update(key, json.loads(value))
